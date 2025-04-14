42a1c7007efed2355b1bee42694004bea339a51c
Nico Diaz <nicolas0405.nd@gmail.com>
2025-04-14 15:51:21 +0000

    tp2_4

--- tp2_4.c
+++ tp2_4.c
@@ -10,6 +10,7 @@ struct {
     char *tipo_cpu; // Apuntador a cadena de caracteres
 } typedef compu;
 
+void cargoDatos(compu pcs[], int cantidad);
 void listarPCs(compu pcs[], int cantidad);
 void mostrarMasVieja(compu pcs[], int cantidad);
 void mostrarMasVeloz(compu pcs[], int cantidad);
@@ -17,24 +18,47 @@ void mostrarMasVeloz(compu pcs[], int cantidad);
 
 int main() 
 {
-    compu PCs[5];
+    //Para generar numeros aleatorios en cada ejecucion
+    srand(time(NULL));
+
+    int cantidadPcs;
+    printf("Ingrese la cantidad de PCs que quiere crear: \n");
+    scanf("%d", &cantidadPcs);
+
+    compu PCs[cantidadPcs];
+    cargoDatos(PCs, cantidadPcs);
+
+    listarPCs(PCs, cantidadPcs);
+
 }
+
+
 void cargoDatos(compu pcs[], int cantidad) 
 {
     for (int i = 0; i < cantidad; i++)
     {
-        pcs[i].velocidad =rand() % (3 - 1 + 1) + 1;
+        pcs[i].velocidad = rand() % (3 - 1 + 1) + 1;
         pcs[i].anio = rand() % (2024 - 2015 + 1) + 2015;
         pcs[i].cantidad_nucleos = rand() % (8 - 1 + 1) + 1;
         
         char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
-        pcs[i].tipo_cpu[3];
+        pcs[i].tipo_cpu = tipos[(rand() % (5 - 0 + 1) + 0)];
     }
     
 }
 
 void listarPCs(compu pcs[], int cantidad)
 {
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("La velocidad del pc %d es %d \n", i+1, pcs[i].velocidad);
+        printf("El anio del pc %d es %d \n", i+1, pcs[i].anio);
+
+        printf("La cantidad de nucleos del pc %d es %d \n", i+1, pcs[i].cantidad_nucleos);
+
+        printf("El tipo de CPU del pc %d es %s \n", i+1, pcs[i].tipo_cpu);
+
+    }
 
 }
 
